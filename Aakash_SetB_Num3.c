9cfd71e5e4819bf8e35022019615ca52fe5ec76c
Aakash109 <98142600+Aakash109@users.noreply.github.com>
2022-02-13 19:40:13 +0530

    Project

---
+++ Aakash_SetB_Num3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{char rems[20];
+int a,i;
+int k=0;
+printf("Enter the number:");
+scanf("%d",&a);
+//convert to binary
+
+while(a>0){
+int rem;
+rem=a%2;
+rems[k]=(char)rem+'0';
+k++;
+a=a/2;}
+printf("Before:");
+
+for(i=0;i<k;i++){
+printf("%c",rems[i]);
+}
+
+printf("\tAfter:");
+for(i=k-1;i>=0;i--){
+printf("%c",rems[i]);
+}
+}
